eb034fd6330fa025edc2d19e58f3e7e8189f17dc
Kyle E <kyle@e2gens.com>
2016-08-25 17:30:28 -0400

    Initial Repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+//about
+
+var middleware={
+  requireAuthenticaton: function(request, response, next){
+    console.log("private route hit");
+    next();
+  },
+  logger: function(request,response,next){
+    console.log(request.method + " " + request.url);
+    next();
+  }
+}
+
+app.use(middleware.logger);
+
+app.get("/about", middleware.requireAuthenticaton, function(request, response){
+  response.send("Hey Hey Hey");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.listen(PORT, function(){
+  console.log("server is started");
+});
